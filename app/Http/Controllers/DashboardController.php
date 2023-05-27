79bb66e9a8ae08e24a023f6763fed53af5598442
Juanda0 <juandavid.echeverri@mercadolibre.com.co>
2023-05-27 14:21:55 -0500

    first chart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Charts\mainChart;
+use App\Models\Student;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         return view('admin.dashboard');
+    }
+
+    public function failedStudentsPerCourse()
+    {   
+        $chart = new mainChart;
+        $data = Student::groupBy('group_code')->select('group_code', DB::raw('count(*) as total'))->get()->pluck('group_code','total');
+        list($keys, $values) = Arr::divide($data);
+        $chart->labels($keys);
+        $chart->dataset('Failed StudentsPerCourse', 'line', $values);
+
+        return view('sample_view', ['chart' => $chart]);
     }
 }
